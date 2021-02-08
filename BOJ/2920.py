bab26b7f681ec435d50abc42f5062fe8a4e8213b
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-08 09:02:05 +0900

    Update 2920.py

--- BOJ/2920.py
+++ BOJ/2920.py
@@ -1,1 +1,20 @@
+arr = input()
+
+arr = arr.split(' ')
+arr = list(map(int,arr))
+
+arr_sorted = sorted(arr)
+arr_reverse = sorted(arr,reverse=True)
+
+
+if arr == arr_sorted:
+    print('ascending')
+
+elif arr == arr_reverse:
+    print('descending')
+
+else:
+    print('mixed')
+
+
 
