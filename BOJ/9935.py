20ec0562167766e31b555f8d81cd69bddde8dcc7
Seulbeen Kim <tmfqksdk@gmail.com>
2021-07-07 19:04:52 +0900

    문자열 폭발 (틀림 2)

--- BOJ/9935.py
+++ BOJ/9935.py
@@ -4,26 +4,22 @@ string_bomb = input()
 
 len_string_input, len_string_bomb = len(string_input), len(string_bomb)
 
-idx_bomb = 0
+idx_bomb, idx_bomb_prev = 0, 0
 string_answer = ""
 for _chr in string_input:
     string_answer += _chr
-    if len(string_answer) > 0 and string_answer[-1] == string_bomb[idx_bomb]:
+    if string_answer[-1] == string_bomb[idx_bomb]:
         idx_bomb += 1
     else:
+        idx_bomb_prev = idx_bomb
         idx_bomb = 0
-        if len(string_answer) > 0 and string_answer[-1] == string_bomb[0]:
+        if string_answer[-1] == string_bomb[0]:
             idx_bomb += 1
 
     if idx_bomb == len_string_bomb:
         string_answer = string_answer[:-len_string_bomb]
-        idx_bomb = 0
-        if len(string_answer) > 0 and string_answer[-1] == string_bomb[0]:
-            idx_bomb += 1
-
-
-
-
+        idx_bomb = idx_bomb_prev
+        idx_bomb_prev = 0
 
 
 if len(string_answer) == 0: string_answer = "FRULA"
