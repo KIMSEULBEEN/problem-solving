0257e36ce6c2dc4bff684d6235be690452bf6b57
Seulbeen Kim <tmfqksdk@gmail.com>
2021-07-08 08:38:22 +0900

    문자열 폭발 (효율성 업그레이드, 아직 미채점)

--- BOJ/9935.py
+++ BOJ/9935.py
@@ -1,19 +1,15 @@
 string_input = input()
 string_bomb = input()
 
-
 len_string_input, len_string_bomb = len(string_input), len(string_bomb)
 
 idx_bomb, idx_bomb_prev = 0, 0
 answer = []
 for _chr in string_input:
     answer.append(_chr)
-    if _chr == string_bomb[-1] and len(answer) >= len_string_bomb:
-        for idx, _chr2 in enumerate(answer[-len_string_bomb:]):
-            if _chr2 != string_bomb[idx]:
-                break
-            if idx == len_string_bomb - 1:
-                del answer[-len_string_bomb:]
+    if _chr == string_bomb[-1]:
+        if ''.join(string_bomb[-len_string_bomb:]) == string_bomb:
+            del answer[-len_string_bomb:]
 
 answer = ''.join(answer)
 if len(answer) == 0: answer = "FRULA"
