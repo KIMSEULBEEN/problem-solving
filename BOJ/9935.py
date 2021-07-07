83f9b079ac832cad0b17df17e4cd695ae323c614
Seulbeen Kim <tmfqksdk@gmail.com>
2021-07-08 08:27:16 +0900

    문자열 폭발 정답 (인터넷 참고)

--- BOJ/9935.py
+++ BOJ/9935.py
@@ -5,23 +5,16 @@ string_bomb = input()
 len_string_input, len_string_bomb = len(string_input), len(string_bomb)
 
 idx_bomb, idx_bomb_prev = 0, 0
-string_answer = ""
+answer = []
 for _chr in string_input:
-    string_answer += _chr
-    if string_answer[-1] == string_bomb[idx_bomb]:
-        idx_bomb += 1
-    else:
-        idx_bomb_prev = idx_bomb
-        idx_bomb = 0
-        if string_answer[-1] == string_bomb[0]:
-            idx_bomb += 1
+    answer.append(_chr)
+    if _chr == string_bomb[-1] and len(answer) >= len_string_bomb:
+        for idx, _chr2 in enumerate(answer[-len_string_bomb:]):
+            if _chr2 != string_bomb[idx]:
+                break
+            if idx == len_string_bomb - 1:
+                del answer[-len_string_bomb:]
 
-    if idx_bomb == len_string_bomb:
-        string_answer = string_answer[:-len_string_bomb]
-        idx_bomb = idx_bomb_prev
-        idx_bomb_prev = 0
-
-
-if len(string_answer) == 0: string_answer = "FRULA"
-
-print(string_answer)
+answer = ''.join(answer)
+if len(answer) == 0: answer = "FRULA"
+print(answer)
