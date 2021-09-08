63309f5078e2d5f595318fbad4c57f428446dba5
Seulbeen Kim <tmfqksdk@gmail.com>
2021-09-08 18:17:10 +0900

    2470 첫 완성

---
+++ BOJ/2470.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+# 입력
+len_numbers = int(input())
+
+# 양의 정수 리스트, 음의 정수 리스트
+plus_numbers, minus_numbers = [], []
+numbers = list(map(int, sys.stdin.readline().split(' ')))
+for number in numbers:
+    if number > 0:
+        plus_numbers.append(number)
+    else:
+        minus_numbers.append(number)
+
+plus_numbers = sorted(plus_numbers)
+minus_numbers = sorted(minus_numbers, reverse=True)
+
+"""
+양의 정수 리스트의 가장 작은 두 수의 합과, 음의 정수 리스트의 가장 작은 두 수의 합 중 작은 값을 초기 answer로 취함
+"""
+sum_plus_2, sum_minus_2 = sum(plus_numbers[:2]), sum(minus_numbers[:2])
+
+is_minus_low = sum_plus_2 > -sum_minus_2
+if is_minus_low:
+    answer = -sum_minus_2
+    answer_list = minus_numbers[:2]
+else:
+    answer = sum_plus_2
+    answer_list = plus_numbers[:2]
+
+plus_numbers, minus_numbers = deque(plus_numbers), deque(minus_numbers)
+number_plus, number_minus = plus_numbers.popleft(), minus_numbers.popleft()
+while answer > 0:
+    # print(number_plus, number_minus)
+    answer_tmp = number_plus + number_minus
+    if abs(answer_tmp) < answer:
+        answer = answer_tmp
+        answer_list = [number_minus, number_plus]
+
+    if number_plus < -number_minus:
+        if len(plus_numbers) == 0: break
+        number_plus = plus_numbers.popleft()
+    else:
+        if len(minus_numbers) == 0: break
+        number_minus = minus_numbers.popleft()
+
+print(*answer_list)
