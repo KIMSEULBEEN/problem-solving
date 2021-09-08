89b97f0f8612bb29df675a261707e88d5bd36ee6
Seulbeen Kim <tmfqksdk@gmail.com>
2021-09-08 18:40:00 +0900

    2470 정답
    음수 배열인 경우 다시 한 번 정렬을 해줘야 하는 부분을 놓쳤다

--- BOJ/2470.py
+++ BOJ/2470.py
@@ -21,28 +21,43 @@ minus_numbers = sorted(minus_numbers, reverse=True)
 """
 sum_plus_2, sum_minus_2 = sum(plus_numbers[:2]), sum(minus_numbers[:2])
 
-is_minus_low = sum_plus_2 > -sum_minus_2
-if is_minus_low:
-    answer = -sum_minus_2
-    answer_list = minus_numbers[:2]
-else:
+if len(plus_numbers) >= 2 and len(minus_numbers) >= 2:
+    is_minus_low = sum_plus_2 > -sum_minus_2
+    if is_minus_low:
+        answer = -sum_minus_2
+        answer_list = minus_numbers[:2]
+    else:
+        answer = sum_plus_2
+        answer_list = plus_numbers[:2]
+
+elif len(plus_numbers) >= 2:
     answer = sum_plus_2
     answer_list = plus_numbers[:2]
 
-plus_numbers, minus_numbers = deque(plus_numbers), deque(minus_numbers)
-number_plus, number_minus = plus_numbers.popleft(), minus_numbers.popleft()
-while answer > 0:
-    # print(number_plus, number_minus)
-    answer_tmp = number_plus + number_minus
-    if abs(answer_tmp) < answer:
-        answer = answer_tmp
-        answer_list = [number_minus, number_plus]
+elif len(minus_numbers) >= 2:
+    answer = sum_minus_2
+    answer_list = minus_numbers[:2]
 
-    if number_plus < -number_minus:
-        if len(plus_numbers) == 0: break
-        number_plus = plus_numbers.popleft()
-    else:
-        if len(minus_numbers) == 0: break
-        number_minus = minus_numbers.popleft()
+else:
+    answer = sys.maxsize
 
-print(*answer_list)
+if len(plus_numbers) > 0 and len(minus_numbers) > 0:
+    plus_numbers, minus_numbers = deque(plus_numbers), deque(minus_numbers)
+    # print(plus_numbers)
+    # print(minus_numbers)
+    number_plus, number_minus = plus_numbers.popleft(), minus_numbers.popleft()
+    # print(number_plus, number_minus, answer)
+    while abs(answer) > 0:
+        answer_tmp = number_plus + number_minus
+        if abs(answer_tmp) < abs(answer):
+            answer = answer_tmp
+            answer_list = [number_minus, number_plus]
+
+        if number_plus < -number_minus:
+            if len(plus_numbers) == 0: break
+            number_plus = plus_numbers.popleft()
+        else:
+            if len(minus_numbers) == 0: break
+            number_minus = minus_numbers.popleft()
+
+print(*sorted(answer_list))
