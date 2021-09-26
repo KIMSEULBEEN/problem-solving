99bf923ba9b09170ec6380766ea4248682d42a9d
Seulbeen Kim <tmfqksdk@gmail.com>
2021-09-26 21:43:27 +0900

    22254 정답(이진탐색)

--- BOJ/22254.py
+++ BOJ/22254.py
@@ -6,7 +6,7 @@ X: 제작 완료까지 남은 시간 [1, 10^9]
 gifts: 선물 별 제작 시간 리스트
 """
 N, X = list(map(int, input().split(' ')))
-gifts = list(map(int, input().split(' ')))
+times = list(map(int, input().split(' ')))
 
 import heapq as hq
 
@@ -14,14 +14,29 @@ def check_possibility(K):
     assembly_lines = [0] * K
     hq.heapify(assembly_lines)
 
-    for gift in gifts:
-        time = hq.heapify(assembly_lines) + gift
-        if time > X: return False
+    for time in times:
+        time_sum = hq.heappop(assembly_lines) + time
+        hq.heappush(assembly_lines, time_sum)
+        if time_sum > X: return False
     return True
 
-start = 0
-end = len(gifts)
-mid = (start + end) // 2
+if len(times) == 1:
+    print(1)
 
-is_produce = False
-while is_produce is False:
+else:
+    low = 1
+    high = len(times)
+    is_possible = False
+    while low <= high:
+        mid = (low + high) // 2
+
+        is_possible = check_possibility(mid)
+        if is_possible:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    if is_possible:
+        print(mid)
+    else:
+        print(mid + 1)
